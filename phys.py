9d3b7821d1b67c8a8878c88b915ada51c61565a9
repri5e <mikhailp2003@mail.ru>
2020-09-25 10:18:50 +0300

    physs

--- phys.py
+++ phys.py
@@ -3,9 +3,9 @@ from graph import *
 radius = 10
 start_point_X = radius/2 
 start_point_Y = 0
-acceleration_Y = 10
-acceleration_X = 1
-velocty_X = 20
+acceleration_Y = 3
+acceleration_X = 0.1
+velocity_X = 10
 velocity_Y = 0
 
 started = False
@@ -21,7 +21,34 @@ arrow = polygon([
         (start_point_X, start_point_Y + 2*radius),
         ])
 
-ball = circle(start_point_X, start_point_Y, radius)
+ball = circle(start_point_X - radius - 10, start_point_Y + radius, radius)
+
+def getCoordinates_X(old_X):
+    global velocity_X
+    if old_X + velocity_X >= 500 - radius:
+        velocity_X = -velocity_X
+        return old_X + velocity_X
+    else:
+        return old_X + velocity_X
+
+
+def getCoordinates_Y(old_Y):
+    global velocity_Y
+    global velocity_X
+    if old_Y + velocity_Y <= 500 - radius:    
+        velocity_Y += acceleration_Y
+        return old_Y + velocity_Y
+    else:
+        velocity_Y = -velocity_Y
+        '''
+        if velocity_X > 0:
+            velocity_X -= acceleration_X
+        elif velocity_X < 0:
+            velocity_X += acceleration_X
+        '''
+        velocity_Y += acceleration_Y
+        return old_Y + velocity_Y
+
 
 def keyPressed(event):
     global started
@@ -42,15 +69,24 @@ def keyPressed(event):
             started = True
             deleteObject(arrow)
     else:
-        if event.keycode == VK_ESCAPE
+        if event.keycode == VK_ESCAPE:
+            close()
 def choose(start_point_Y):
+    global ball
     penColor("yellow")
     brushColor("yellow")
     moveObjectTo(arrow, start_point_X, start_point_Y)
+    deleteObject(ball)
+    ball = circle(start_point_X - radius - 10, start_point_Y + radius, radius)
 
-def moveBall(directX, directY):
-    new_X = getCoordinates()
-    #TODO: add others
+def moveBall():
+    if started:
+        old_X = xCoord(ball) + radius
+        old_Y = yCoord(ball) + radius
+        new_X = getCoordinates_X(old_X)
+        new_Y = getCoordinates_Y(old_Y)
+        moveObjectTo(ball, new_X, new_Y)
 
+onTimer(moveBall, 30)
 onKey(keyPressed)
 run()
